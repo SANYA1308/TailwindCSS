e9add018f37fc4ec6213ce156e362c35e7fd1fb9
Sanya <sanyagondi1308@gmail.com>
2023-05-25 22:40:08 +0530

    TailwindCSS

---
+++ tailwind/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+   <div class='h-screen grid place-items-center'>
+
+  
+    
+    <h1 class='text-gray-950 font-bold uppercase'>App</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
