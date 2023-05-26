8b129e1c81791496a86c9d2db0d1ba4950912b21
Sanya <sanyagondi1308@gmail.com>
2023-05-26 16:35:22 +0530

    height and width

--- tailwind/src/App.js
+++ tailwind/src/App.js
@@ -2,14 +2,36 @@ import React from 'react'
 
 const App = () => {
   return (
-    <>
-   <div class='h-screen grid place-items-center'>
+  //   <>
+  //  {/* <div class=' grid place-items-center h-screen'>
 
   
     
-    <h1 class='text-gray-950 font-bold uppercase'>App</h1>
-    </div>
-    </>
+  //   {/* <h1 class='text-gray-950 font-bold uppercase'>Tailwind CSS</h1> */}
+
+  //   <h1 class="h-20 w-1/2 bg-blue-200">Using Tailwind CSS</h1>
+  //   </div> */}
+
+  //   </>
+<>
+{/* <div class="grid place-items-center h-screen">
+
+  <div class="flex w-full">
+  <div class="w-1/2 bg-blue-200">1</div>
+  <div class="w-1/2 bg-blue-400">2</div> 
+  <div class="w-1/2 bg-blue-600">3</div> 
+
+</div>
+</div> */}
+
+<div class="flex h-screen">
+  <div class="w-1/5 bg-blue-200">1</div>
+  <div class="w-2/5 bg-blue-400">2</div> 
+  <div class="w-3/5 bg-blue-600">3</div> 
+  <div class="w-2/5 bg-blue-800">4</div> 
+  <div class="w-4/5 bg-blue-300">5</div> 
+</div>
+</>
   )
 }
 
