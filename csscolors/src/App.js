96eadd61d0c73f1fd801c0f0bf288a325b08db22
Sanya <sanyagondi1308@gmail.com>
2023-05-26 17:07:10 +0530

    csscolors and font styling

---
+++ csscolors/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+   <>
+   <div class=
+   "text-green-100 text-2xl font-mono place-items-center w-40 h-20  bg-blue-900">
+    Tailwind CSS
+   </div>
+   
+   
+   
+   
+   
+   </>
+  )
+}
+
+export default App
